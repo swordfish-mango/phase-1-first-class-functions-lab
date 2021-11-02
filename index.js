2fc59f67e9675585c3f9db4f43fd8cf6b1837610
Marcus <marcus.shimotsu@gmail.com>
2021-11-01 19:27:56 -0500

    commit tree 595b748a6924958faeb23daeeae4263e64523184

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (callbackArray) => [callbackArray[0],callbackArray[1]];
+const returnLastTwoDrivers = (callbackArray) => [callbackArray[callbackArray.length-2],callbackArray[callbackArray.length-1]]; 
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+const createFareMultiplier = (int) => function(multiplier) {return multiplier*int; };
+const fareDoubler = (fare) => createFareMultiplier(fare)(2);
+const fareTripler = (fare) => createFareMultiplier(fare)(3);
+const selectDifferentDrivers = (arrayOfDrivers, driverFunction) => driverFunction(arrayOfDrivers);
